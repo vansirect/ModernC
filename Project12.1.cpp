031299f7780c5cfba1e2305b57a1af20e3afd795
Tz <75564936+vansirect@users.noreply.github.com>
2025-03-03 15:57:57 +0300

    Add files via upload

---
+++ Project12.1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+
+int main() {
+	int character;
+	int arr[40];
+	int i = 0;
+	printf("Enter a message: ");
+	while ((character = getchar()) != '\n' && i < 40) {
+		arr[i] = character;
+		i++;
+	}
+	printf("Reversal is: ");
+	for (int j = i - 1; j >= 0;j--) {
+		printf("%c", arr[j]);
+		
+	}
+	
+	return 0;
+}	
