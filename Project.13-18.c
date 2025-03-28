2ac08048716b2e71eb17df889b0c37e0f4224d83
Tz <75564936+vansirect@users.noreply.github.com>
2025-03-28 15:54:55 +0300

    Add files via upload

---
+++ Project.13-18.c
@@ -0,0 +1,28 @@
+﻿#include<stdio.h>
+#include<string.h>
+
+
+
+
+char* months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"};
+
+int main() {
+    int m, d, y;
+    char date[20];
+    char month[20];
+    printf("Enter a date (mm/dd/yyyy): ");
+
+    scanf("%s", &date);
+
+    sscanf(date, "%d/%d/%d", &m, &d, &y);
+     
+    strcpy(month, months[m - 1]);
+
+    printf("You entered the date %s %d %d", month, d, y);
+
+
+
+    
+}
